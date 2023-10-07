b357d93019fbd870a746d6d9da4105d6eee82a85
Joao Ricardo Rodrigues de Moraes <joao.dsn.shop@alterdata.com.br>
2023-10-07 18:25:54 -0300

    Antes conversão para projeto Vite

--- src/index.js
+++ src/index.js
@@ -1,30 +1,65 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import ReactDOM from 'react-dom';
 import './index.css';
-import { ThemeProvider, createTheme } from '@mui/material';
+//import { ThemeProvider, createTheme } from '@mui/material';
+//import { ThemeProvider } from 'styled-components';
+//import { createTheme } from '@mui/material';
 //import { StylesProvider } from '@mui/styles';
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Routes from 'pages/Routes';
 
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#2A9F85'
-		},
-		secondary: {
-			main: '#FF7070'
-		},
-	}
-})
+// const theme = createTheme({
+// 	palette: {
+// 		primary: {
+// 			main: '#2A9F85'
+// 		},
+// 		secondary: {
+// 			main: '#FF7070'
+// 		},
+// 	}
+// })
 
 
-ReactDOM.render(
+function App() {
+	return (
+		<Routes />
+	)
+}
+
+
+// function App() {
+// 	return (
+// 		<React.StrictMode>
+// 			{/* <StylesProvider injectFirst> */}
+// 			<ThemeProvider theme={theme}>
+// 				<Routes />
+// 			</ThemeProvider>
+// 			{/* </StylesProvider> */}
+// 		</React.StrictMode>
+// 	)
+// }
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <StylesProvider injectFirst> */}
-		<ThemeProvider theme={theme}>
-			<Routes />
-		</ThemeProvider>
-		{/* </StylesProvider> */}
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+		<App />
+	</React.StrictMode>
+)
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>,
+// )
+
+
+
+
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<Routes />
+// 	</React.StrictMode>,
+// 	document.getElementById('root')
+// );
